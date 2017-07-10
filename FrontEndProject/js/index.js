ec9f4cb10d409555c1db9974bb132c31ca1cad93
anshul0888 <anshulsharma0888@gmail.com>
2017-07-10 15:55:01 +0530

    Project Problem

---
+++ FrontEndProject/js/index.js
@@ -0,0 +1,5 @@
+var app = angular.module('Application',[]);
+app.config()
+app.controller('MainController',function($scope){
+    
+});
---
+++ FrontEndProject/js/register.js
@@ -0,0 +1,4 @@
+var app = angular.module("register",['ngMessages']);
+app.controller('MainController',function($scope){
+    
+});
